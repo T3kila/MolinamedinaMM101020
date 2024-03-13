c58f40b6a576547928231721aac6f1d053fdbe5d
T3kila <Diegoalfmolinamail.com>
2024-03-12 19:57:32 -0600

    primer push

---
+++ actionBar05-03-2024/app/src/main/java/com/example/alvaradomoran/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.alvaradomoran
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val toolbar= findViewById<Toolbar>(R.id.toolbar)
+
+
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.title="Molina Medina"
+        supportActionBar?.subtitle="Universidad Francisco Gavidia"
+        supportActionBar?.setIcon(R.drawable.ic_launcher_foreground)
+
+        val boton = findViewById<Button>(R.id.btn);
+        boton.setOnClickListener{
+            val  intent = Intent(this, SegundaPantalla::  class.java)
+            startActivity(intent)
+
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater .inflate(R.menu.ufg_toolbar,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.configuracion->{
+                Toast.makeText( this, "usted ha seleccionado la opcion de configuracion ",Toast.LENGTH_LONG).show()
+            true
+        }
+            R.id.perfil->{
+                Toast.makeText( this, "usted ha seleccionado la opcion de perfil ",Toast.LENGTH_LONG).show()
+                true
+            }
+            R.id.mapa->{
+                Toast.makeText( this, "usted ha seleccionado la opcion de su ubicacion ",Toast.LENGTH_LONG).show()
+                true
+            }
+            R.id.nueva_cuenta->{
+                Toast.makeText( this, "usted ha seleccionado la opcion de una nueva cuenta ",Toast.LENGTH_LONG).show()
+                true
+            }
+            R.id.salir->{
+                Toast.makeText( this, "usted ha seleccionado la opcion de salir de la app ",Toast.LENGTH_LONG).show()
+                true
+            }
+            else -> false
+        }
+
+    }
+
+}
---
+++ actionBar05-03-2024/app/src/main/java/com/example/alvaradomoran/SegundaPantalla.kt
@@ -0,0 +1,63 @@
+package com.example.alvaradomoran
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+
+class SegundaPantalla : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_segunda_pantalla)
+
+        val toolbar= findViewById<Toolbar>(R.id.toolbar)
+
+
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.title="Molina Medina"
+        supportActionBar?.subtitle="Universidad Francisco Gavidia"
+        supportActionBar?.setIcon(R.drawable.ic_launcher_foreground)
+
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return true
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater .inflate(R.menu.ufg_toolbar,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.configuracion->{
+                Toast.makeText( this, "usted ha seleccionado la opcion de configuracion ", Toast.LENGTH_LONG).show()
+                true
+            }
+            R.id.perfil->{
+                Toast.makeText( this, "usted ha seleccionado la opcion de perfil ", Toast.LENGTH_LONG).show()
+                true
+            }
+            R.id.mapa->{
+                Toast.makeText( this, "usted ha seleccionado la opcion de su ubicacion ", Toast.LENGTH_LONG).show()
+                true
+            }
+            R.id.nueva_cuenta->{
+                Toast.makeText( this, "usted ha seleccionado la opcion de una nueva cuenta ", Toast.LENGTH_LONG).show()
+                true
+            }
+            R.id.salir->{
+                Toast.makeText( this, "usted ha seleccionado la opcion de salir de la app ", Toast.LENGTH_LONG).show()
+                true
+            }
+            else -> false
+        }
+
+    }
+}
